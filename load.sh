3b94cb5a52e22c62b55fae056b5a03d20439c9aa
van Hauser <vh@thc.org>
2020-09-11 14:42:38 +0200

    enhance module build

--- load.sh
+++ load.sh
@@ -4,14 +4,10 @@ if [ '!' "$EUID" = 0 ] && [ '!' `id -u` = 0 ] ; then
 	echo "Warning: you need to be root to run this!"
 	# we do not exit as other mechanisms exist that allows to do this than
 	# being root. let the errors speak for themselves.
-	exit 1
 fi
-
-export ARCH=${ARCH:-x64}
 
 cd src/
 
 rmmod afl_snapshot
-
 make
-insmod afl_snapshot.ko
+insmod afl_snapshot.ko && echo Successfully loaded the snapshot module
